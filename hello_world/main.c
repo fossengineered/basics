8ff35c46acd49299f5f95ca3e1b6bd2b829b9fa2
fossengineered <99218408+fossengineered@users.noreply.github.com>
2022-03-07 19:06:52 -0500

    Update main.c

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,7 +1,18 @@
+// Simple C program to display "Hello World"
+  
+// Header file for input output functions
 #include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hello world
+    printf("Hello World");
 
-int main(){
-    printf("Hello World!");
-
+    //stop application closing by waiting for any input from the user
+    getchar();
+  
     return 0;
 }
